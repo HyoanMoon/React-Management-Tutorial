c5a1a705ff42ff9083295312a9d4afc3b5184112
Hyoan Moon <tjdud24as@gmail.com>
2024-12-15 20:49:34 -0500

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Customer from "./components/Customer";
 import {
@@ -9,38 +10,22 @@ import {
   Paper,
 } from "@mui/material";
 
-
-const customers = [
-  {
-    id: "1",
-    image:
-      "https://media.licdn.com/dms/image/v2/D4D03AQGaishnSDUgDQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1675805436538?e=1739404800&v=beta&t=dup3UpTmUqdY9aAnZ8WitM-Extu3jLSFYlG5Z7anAPA",
-    name: "Hyoan Moon",
-    birthday: "990204",
-    gender: "Women",
-    job: "Student",
-  },
-  {
-    id: "2",
-    image:
-      "https://preview.redd.it/dztoznt0rvr31.jpg?auto=webp&s=dda057ae335d4a23d6813bd4e9214335491586e7",
-    name: "Jennie",
-    birthday: "960204",
-    gender: "Women",
-    job: "Singer",
-  },
-  {
-    id: "3",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Blackpink_Ros%C3%A9_Rimowa_1.jpg/220px-Blackpink_Ros%C3%A9_Rimowa_1.jpg",
-    name: "Rosie",
-    birthday: "970204",
-    gender: "Women",
-    job: "Singer",
-  },
-];
-
 function App() {
+  const [customers, setCustomers] = useState([]); // 초기값을 빈 배열로 설정
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/customers");
+        const res = await response.json();
+        setCustomers(res);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <Paper
       sx={{
--- server.js
+++ server.js
@@ -6,8 +6,37 @@ const port = process.env.PORT || 4000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello Express!" });
+// 전체 고객 목록을 불러오는 api
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: "1",
+      image:
+        "https://media.licdn.com/dms/image/v2/D4D03AQGaishnSDUgDQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1675805436538?e=1739404800&v=beta&t=dup3UpTmUqdY9aAnZ8WitM-Extu3jLSFYlG5Z7anAPA",
+      name: "Hyoan Moon",
+      birthday: "990204",
+      gender: "Women",
+      job: "Student",
+    },
+    {
+      id: "2",
+      image:
+        "https://preview.redd.it/dztoznt0rvr31.jpg?auto=webp&s=dda057ae335d4a23d6813bd4e9214335491586e7",
+      name: "Jennie",
+      birthday: "960204",
+      gender: "Women",
+      job: "Singer",
+    },
+    {
+      id: "3",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Blackpink_Ros%C3%A9_Rimowa_1.jpg/220px-Blackpink_Ros%C3%A9_Rimowa_1.jpg",
+      name: "Rosie",
+      birthday: "970204",
+      gender: "Women",
+      job: "Singer",
+    },
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
