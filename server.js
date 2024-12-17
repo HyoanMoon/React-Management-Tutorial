abaa5ff3b0c202fb40629bcc4570d43d41576d52
Hyoan Moon <tjdud24as@gmail.com>
2024-12-16 20:39:22 -0500

    Create Customer Database Table & Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,37 +7,25 @@ const port = process.env.PORT || 4000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+connection.connect();
+
 // 전체 고객 목록을 불러오는 api
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: "1",
-      image:
-        "https://media.licdn.com/dms/image/v2/D4D03AQGaishnSDUgDQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1675805436538?e=1739404800&v=beta&t=dup3UpTmUqdY9aAnZ8WitM-Extu3jLSFYlG5Z7anAPA",
-      name: "Hyoan Moon",
-      birthday: "990204",
-      gender: "Women",
-      job: "Student",
-    },
-    {
-      id: "2",
-      image:
-        "https://preview.redd.it/dztoznt0rvr31.jpg?auto=webp&s=dda057ae335d4a23d6813bd4e9214335491586e7",
-      name: "Jennie",
-      birthday: "960204",
-      gender: "Women",
-      job: "Singer",
-    },
-    {
-      id: "3",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Blackpink_Ros%C3%A9_Rimowa_1.jpg/220px-Blackpink_Ros%C3%A9_Rimowa_1.jpg",
-      name: "Rosie",
-      birthday: "970204",
-      gender: "Women",
-      job: "Singer",
-    },
-  ]);
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+    // console.log(fields);
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
